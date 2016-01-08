02dcc27290ba442bfa08e90166b646cab4a97f9c
Jesse Monroy <jessemonroy650@yahoo.com>
2016-01-08 06:12:10 -0700

    update

--- app.js
+++ app.js
@@ -85,11 +85,13 @@ var app = {
 // This is to have the 'deviceready' code from firing in a webbrowser.
 var device = {platform:'browser'};
 
+document.getElementById('appState').innerHTML = "dom loaded";
 // Thanks http://www.quirksmode.org/js/detect.html
 if ('mozApps' in navigator) {
     document.addEventListener("DOMContentLoaded", app.onDeviceReady, false);
     document.getElementById("product").innerHTML = "got mozApps";
 } else {
     document.addEventListener("deviceready", app.onDeviceReady, false);
-    document.getElementById("product").innerHTML = JSON.stringify(navigator);
+    document.getElementById("product").innerHTML  = JSON.stringify(navigator);
+    document.getElementById('appState').innerHTML = "addEventListener - deviceready";
 }
