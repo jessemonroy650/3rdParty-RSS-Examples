1f687ff1a30d1e825a83583250ebdd40551e70ce
Jesse Monroy <jessemonroy650@yahoo.com>
2016-01-09 02:24:45 -0700

    try again

--- app.js
+++ app.js
@@ -34,10 +34,12 @@ var app = {
         document.addEventListener("pause",  app.onPause, false);
         document.addEventListener("resume", app.onResume, false);
         // The versions we define
+/*
         if ('AppVersion' in window) {
             document.getElementById('appversion').innerHTML   = AppVersion.version;
             document.getElementById('buildversion').innerHTML = AppVersion.build;
         }
+*/
         // Write device information to screen
         document.getElementById('cordova').innerHTML = device.cordova;
         document.getElementById('model').innerHTML   = device.model;
