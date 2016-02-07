baecb72df72f99112e8acddef37663303e753f0f
Jesse Monroy <jessemonroy650@yahoo.com>
2016-02-07 01:17:20 -0700

    try

--- js/rss-reader.js
+++ js/rss-reader.js
@@ -44,7 +44,7 @@ var readerApp = {
             //
             var title         = xml.find( "title" );
             //$('#dbug').html(title + '<br>' + title[0]);
-            var mtitle        = xml.closest( "title" );
+            var mtitle        = xml.next( "title" );
             $('#dbug').html(mtitle);
             var xtitle        = title[0]; // bug in jquery - 2016-01-12
             var description   = xml.find( "description" );
