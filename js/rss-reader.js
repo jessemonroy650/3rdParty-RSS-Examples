88cddfd2600e04c29510ccb6f97553ff1a13cd93
Jesse Monroy <jessemonroy650@yahoo.com>
2016-03-08 18:49:41 -0700

    try removing differnet place

--- js/rss-reader.js
+++ js/rss-reader.js
@@ -30,8 +30,7 @@ var readerApp = {
             // let's see if it will parse
             try {
                 // replace(/(^<media:title .*$)/gm, "")
-                // try to remove offending line.
-                data.replace(/(^<media:title .*$)/gm, "");
+                // try to remove offending line. Did not work here. 2016-03-08T18:47:16
                 var xml           = $( data );
                 externalElements['status']('xml make object okay');
             }
@@ -51,6 +50,8 @@ var readerApp = {
         });
     },
     parseXML(xml, extEl) {
+        // Try removing offending line
+        xml.replace(/(^<media:title .*$)/gm, "");
         extEl['status']('parsing xml');
         var title         = xml.find( "title" );
         //$('#dbug').html(title + '<br>' + title[0]);
